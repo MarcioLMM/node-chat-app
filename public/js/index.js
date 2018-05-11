aa5d40e5cd668127bcd89125cb7866328a027a10
Marcio Macedo <marcioluismacedo@gmail.com>
2018-05-10 21:31:21 -0300

    Fixing errors

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+var socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnect from server');
+});
+
+socket.on('newMessage', function (message) {
+    console.log('New Message', message);
+});
+
